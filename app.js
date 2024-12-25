66a00029a00228d0751106a6d1a10d40764ceda1
Daniel Rodrigues <danielvcr@outlook.com>
2024-12-24 22:55:08 -0300

    ALTERANDO AUDIO

--- app.js
+++ app.js
@@ -5,7 +5,14 @@ let tentativa = 1;
 function exibirTextoNaTela(tag,texto){
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
-    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
+    if ('speechSynthesis' in window) {
+        let utterance = new SpeechSynthesisUtterance(texto);
+        utterance.lang = 'pt-BR'; 
+        utterance.rate = 1.2; 
+        window.speechSynthesis.speak(utterance); 
+    } else {
+        responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
+    }
 }
 function mensagemInicial(){
     exibirTextoNaTela('h1','Jogo do número secreto!');
